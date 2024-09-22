49ce9ccfc599dd9384688e76df62faf43dc75193
Indrajeet Patil <Indrajeetspatil2002@gmail.com>
2024-09-22 20:48:56 +0530

    Update PrivateRoute.jsx ignore
    ignore

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
@@ -8,4 +8,4 @@ const PrivateRoute = ({ children }) => {
   return currentUser ? children : <Navigate to="/login" />;
 };
 
-export default PrivateRoute;
+export default PrivateRoute;*/
